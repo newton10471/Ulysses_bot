264f2d18eed9aa71a2b3f1286349ea141d20168f
Matt Newton <newton10471@gmail.com>
2012-07-21 20:57:12 -0400

    made Litbot class and examples, and + method for bots work

--- ulysses_bot.rb
+++ ulysses_bot.rb
@@ -1,4 +1,6 @@
 class LitBot
+  attr_accessor :word_pairs_and_probabilities, :source_text
+
   def eat_file(file_name)
     file = File.open(file_name)
     @source_text = file.read
@@ -11,39 +13,80 @@ class LitBot
 
   def digest_file
     source_text_words = @source_text.split(' ')
-    word_pairs_and_probabilities = {}
+    @word_pairs_and_probabilities = {}
 
     source_text_words.each_with_index do |word, index|
       hash_key = "#{word} #{source_text_words[index + 1]}"
       hash_value = source_text_words[index + 2]
-      if word_pairs_and_probabilities[hash_key]
-        word_pairs_and_probabilities[hash_key] << hash_value
+      if @word_pairs_and_probabilities[hash_key]
+        @word_pairs_and_probabilities[hash_key] << hash_value
       else
-        word_pairs_and_probabilities[hash_key] = [hash_value]
+        @word_pairs_and_probabilities[hash_key] = [hash_value]
       end
     end
   end
 
-  def speak
-    puts word_pairs_and_probabilities.first
+  def speak_first_word
+    puts @word_pairs_and_probabilities.first
   end
+
+  def pick_first_two_words_slow_but_random
+    two_words = []
+    while (@word_pairs_and_probabilities[two_words.join(' ')] == nil) do
+      two_words = @source_text.split(' ').sample(2)
+      two_words[0] = two_words[0].capitalize
+    end
+    return two_words
+  end
+
+  def pick_first_two_words
+    two_words = []
+    two_words = @source_text.split(' ').first(2)
+    return two_words
+  end
+
+  # num is number of sentences
+  def speak(num)
+    new_text = pick_first_two_words
+    while (num > 0) do
+      hash_key = "#{new_text[-2]} #{new_text[-1]}"
+      new_word = @word_pairs_and_probabilities[hash_key].sample 
+      new_text <<  new_word unless new_word == nil
+      if /\./.match(new_word)
+        num -= 1 
+      end
+    end
+    return new_text.join(" ")
+  end
+
+  def +(added_bot)
+    result = dup
+    result.source_text = result.source_text + added_bot.source_text
+    result.word_pairs_and_probabilities = result.word_pairs_and_probabilities.merge(added_bot.word_pairs_and_probabilities)
+    return result
+  end
+
 end
 
+paragraph_size = 10
 bender = LitBot.new
 bender.eat_file("huckle.txt")
 # bender.speak_first_10_words # an example call
 bender.digest_file
-bender.speak
+puts bender.speak(paragraph_size)
+# p "#{bender.object_id} #{bender.word_pairs_and_probabilities.size}"
 
 ulysses_bot = LitBot.new
 ulysses_bot.eat_file("ulysses.txt")
 # ulysses.speak_first_10_words # an example call
 ulysses_bot.digest_file
-ulysses_bot.speak
+puts ulysses_bot.speak(paragraph_size)
+# p "#{ulysses_bot.object_id} #{ulysses_bot.word_pairs_and_probabilities.size}"
 
 # BONUS HW
 hybrid_bot = ulysses_bot + bender
-hybrid_bot.speak
+puts hybrid_bot.speak(paragraph_size)
+# p "#{hybrid_bot.object_id} #{hybrid_bot.word_pairs_and_probabilities.size}"
 
 # ulysses = File.open("ulysses.txt")
 # source_text = ulysses.read
