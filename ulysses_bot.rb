f32e37c848861240178959ecc3abce1db0d508e6
Matt Newton <newton10471@gmail.com>
2012-07-26 19:09:18 -0400

    sinatrified the Ulysses bot

--- ulysses_bot.rb
+++ ulysses_bot.rb
@@ -1,3 +1,5 @@
+require 'sinatra'
+
 class LitBot
   # attr_accessor :word_pairs_and_probabilities, :source_text
 
@@ -107,6 +109,8 @@ class LitBot
 
 end
 
+
+
 paragraph_size = 10
 bender = LitBot.new
 bender.eat_file("huckle.txt")
@@ -127,34 +131,14 @@ hybrid_bot = ulysses_bot + bender
 puts hybrid_bot.speak(paragraph_size)
 # p "#{hybrid_bot.object_id} #{hybrid_bot.word_pairs_and_probabilities.size}"
 
-# ulysses = File.open("ulysses.txt")
-# source_text = ulysses.read
-# ulysses.close
+get '/hi' do
+    "Hello World"
+end
 
-# source_text_words = source_text.split(' ')
-# word_pairs_and_probabilities = {}
+get '/hi-matt' do
+  "Hi there, Matt!"
+end
 
-# source_text_words.each_with_index do |word, index|
-  # hash_key = "#{word} #{source_text_words[index + 1]}"
-  # hash_value = source_text_words[index + 2]
-  # if word_pairs_and_probabilities[hash_key]
-    # word_pairs_and_probabilities[hash_key] << hash_value
-  # else
-    # word_pairs_and_probabilities[hash_key] = [hash_value]
-  # end
-# end
-
-# # puts word_pairs_and_probabilities
-# output_text = ['They', 'came']
-
-# story = 0
-# while story < 35 do
-  # word_pair = output_text.last(2).join(' ')
-  # next_word = word_pairs_and_probabilities[word_pair].sample unless word_pairs_and_probabilities[word_pair].nil?
-  # output_text << next_word
-
-  # if next_word && (next_word.include?(".") || next_word.include?("?"))
-    # story += 1
-  # end
-# end
-# puts output_text.join(' ')
+get '/hybrid_bot' do
+  hybrid_bot.speak(paragraph_size)
+end
